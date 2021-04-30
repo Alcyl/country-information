c8bb989aba683326ead0178e2669fed251c920d6
mariotripoli <mario.tripoli91@gmail.com>
2021-04-30 22:29:44 +0200

    added chart lib and index.html

---
+++ src/main/resources/static/js/chart-action.js
@@ -0,0 +1,30 @@
+var ctx = document.getElementById('myChart');
+
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data:  {
+  labels: [
+    'Red',
+    'Blue',
+    'Yellow'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+//TODO: fill with data from corona API
+    data: [300, 50, 100],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }]
+},
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
