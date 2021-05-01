f45077523d4bbb1c40f45f2add1df5e4582f04d4
mariotripoli <mario.tripoli91@gmail.com>
2021-05-01 20:49:49 +0200

    changed annotation for data usage in thymeleaf and index method

--- src/main/java/com/mario/countrycatalog/rescources/CountryCatalogResource.java
+++ src/main/java/com/mario/countrycatalog/rescources/CountryCatalogResource.java
@@ -5,10 +5,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mario.countrycatalog.models.Corona.CoronaInformation;
 import com.mario.countrycatalog.models.Country.CountryInformation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
@@ -16,7 +17,8 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
-@RestController
+// @RestController
+@Controller
 public class CountryCatalogResource {
 
     @Autowired
@@ -34,6 +36,7 @@ public class CountryCatalogResource {
     public final String urlCorona = "https://api.covid19api.com/";
 
     @RequestMapping("/countries")
+    @ResponseBody
     public List<CountryInformation> getAllCountries() {
         CountryInformation[] countries = restTemplate.getForObject(urlCountry + "all", CountryInformation[].class);
 
@@ -41,7 +44,9 @@ public class CountryCatalogResource {
         return Arrays.asList(countries);
     }
 
+    // TODO: Get countryName from client inputfield
     @RequestMapping("/countries/{countryName}")
+    @ResponseBody
     public CountryInformation[] getCountry(@PathVariable String countryName, Model model) {
         try {
             // ggf Model raus
@@ -65,19 +70,26 @@ public class CountryCatalogResource {
         return restTemplate.getForObject(urlCountry + "name/" + countryName, CountryInformation[].class);
     }
 
-//    @RequestMapping("/test")
-//    public String test(Model model) {
-//        model.addAttribute("countryInformation", new CountryInformation());
-//        return "countryInformation";
-//    }
-
-
-
     @RequestMapping("/corona/summary")
+    @ResponseBody
     public CoronaInformation getAllCoronaInformations() {
+        CoronaInformation coronaInformation = restTemplate.getForObject(urlCorona + "summary", CoronaInformation.class);
+        assert coronaInformation != null;
+        System.out.println(coronaInformation.getGlobal().getNewConfirmed());
 
-        return restTemplate.getForObject(urlCorona + "summary", CoronaInformation.class);
+
+        return coronaInformation;
     }
+
+    @RequestMapping("/")
+    public String test(Model model) {
+        CoronaInformation coronaInformation = restTemplate.getForObject(urlCorona + "summary", CoronaInformation.class);
+//        System.out.println(coronaInformation.getGlobal().getNewConfirmed());
+        model.addAttribute("coronaInformation", coronaInformation);
+
+        return "index";
+    }
+
 //    TODO: GET Live By Country All Status
     //TODO: GET Live By Country And Status After Date
 }
