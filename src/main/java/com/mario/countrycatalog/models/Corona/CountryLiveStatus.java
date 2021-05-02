92076d6f131f3111f15f64cbb47c55e88dbb54cf
mariotripoli <mario.tripoli91@gmail.com>
2021-05-02 18:16:13 +0200

    added new model

---
+++ src/main/java/com/mario/countrycatalog/models/Corona/CountryLiveStatus.java
@@ -0,0 +1,121 @@
+package com.mario.countrycatalog.models.Corona;
+
+public class CountryLiveStatus {
+    public String ID;
+    public String Country;
+    public String CountryCode;
+    public String Province;
+    public String City;
+    public String CityCode;
+    public String Lat;
+    public String Lon;
+    public int Confirmed;
+    public int Deaths;
+    public int Recovered;
+    public int Active;
+    public java.util.Date Date;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public String getCountryCode() {
+        return CountryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        CountryCode = countryCode;
+    }
+
+    public String getProvince() {
+        return Province;
+    }
+
+    public void setProvince(String province) {
+        Province = province;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public String getCityCode() {
+        return CityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        CityCode = cityCode;
+    }
+
+    public String getLat() {
+        return Lat;
+    }
+
+    public void setLat(String lat) {
+        Lat = lat;
+    }
+
+    public String getLon() {
+        return Lon;
+    }
+
+    public void setLon(String lon) {
+        Lon = lon;
+    }
+
+    public int getConfirmed() {
+        return Confirmed;
+    }
+
+    public void setConfirmed(int confirmed) {
+        Confirmed = confirmed;
+    }
+
+    public int getDeaths() {
+        return Deaths;
+    }
+
+    public void setDeaths(int deaths) {
+        Deaths = deaths;
+    }
+
+    public int getRecovered() {
+        return Recovered;
+    }
+
+    public void setRecovered(int recovered) {
+        Recovered = recovered;
+    }
+
+    public int getActive() {
+        return Active;
+    }
+
+    public void setActive(int active) {
+        Active = active;
+    }
+
+    public java.util.Date getDate() {
+        return Date;
+    }
+
+    public void setDate(java.util.Date date) {
+        Date = date;
+    }
+}
